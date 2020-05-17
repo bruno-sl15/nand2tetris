e13c146335388e1ec6539a989a2ae2b81d7398a2
Bruno Leite <bruno.silvaleite15@gmail.com>
2020-05-17 19:26:45 -0300

    Parser Class implementation

--- 06/HackAssembler/Parser.py
+++ 06/HackAssembler/Parser.py
@@ -1,3 +1,6 @@
+import re
+
+
 class Parser(object):
     def __init__(self):
         self._a_instruction = ''
@@ -8,3 +11,24 @@ class Parser(object):
 
     def set_c_instruction(self, instruction):
         self._c_instruction = instruction
+
+    def comp(self):
+        if '=' in self._c_instruction:
+            return re.split('[=;]', self._c_instruction)[1]
+        else:
+            return re.split('[=;]', self._c_instruction)[0]
+
+    def dest(self):
+        if '=' in self._c_instruction:
+            return re.split('[=;]', self._c_instruction)[0]
+        else:
+            return ''
+
+    def jump(self):
+        if ';' in self._c_instruction:
+            return re.split('[=;]', self._c_instruction)[-1]
+        else:
+            return ''
+
+    def number(self):
+        return int(self._a_instruction[1:])
