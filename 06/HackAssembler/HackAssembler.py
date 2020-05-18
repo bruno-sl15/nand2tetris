e4c8a8e047b3a72b4efeabc81d18d74ffb177352
Bruno Leite <bruno.silvaleite15@gmail.com>
2020-05-18 16:00:31 -0300

    Iterative pass close #1

--- 06/HackAssembler/HackAssembler.py
+++ 06/HackAssembler/HackAssembler.py
@@ -11,67 +11,68 @@ class HackAssembler:
         asm_file = open(file_name, 'r')  # open the asm file
         self._instructions = asm_file.read().split('\n')  # initialize a list with the assembly instructions
         asm_file.close()  # close the asm file
-        self._line = 0  # assembly instructions line counter
         self._hack_file_name = file_name.split('.asm')[0] + '.hack'  # name of the output hack file
-        self._hack_file = open(self._hack_file_name, 'w')  # opens the output hack file in writing mode
-        self._hack_file = open(self._hack_file_name, 'r')  # opens the output hack file in reading mode
-        self._machine_code = self._hack_file.readlines()  # initialize a list for the output hack instructions
+        self._machine_code = []  # initialize a list for the output hack instructions
         self._pass()
 
-    # recursively iterate and translate each assembly instruction to hack machine code
+    # iterate and translate each assembly instruction to hack machine code
     def _pass(self):
-        # keep iterating if there still remain instructions
-        if self._line < len(self._instructions):
+        for line in self._instructions:
             # translate if the current line is a real instruction
-            if self._is_instruction():
-                instruction = self._remove_comment()
+            if self._is_instruction(line):
+                instruction = self._remove_comment(line)
                 if self._is_c_instruction(instruction):  # translate a C-Instruction
-                    self._parser.set_c_instruction(instruction)
-                    c = self._parser.comp()
-                    d = self._parser.dest()
-                    j = self._parser.jump()
-                    cc = self._code.comp(c)
-                    dd = self._code.dest(d)
-                    jj = self._code.jump(j)
-                    out = '111' + cc + dd + jj
+                    comp, dest, jump = self._translate_c_instruction(instruction)
+                    out = '111' + comp + dest + jump
                 else:  # translate a A-Instruction
-                    self._parser.set_a_instruction(instruction)
-                    n = self._parser.number()
-                    nn = self._code.number(n)
-                    out = '0' + nn
+                    number = self._translate_a_instruction(instruction)
+                    out = '0' + number
                 self._machine_code.append(out+'\n')
-            self._line += 1  # increment the line counter
-            self._pass()  # move to the next recursive iteration
-        else:  # finish the iterations
-            self._hack_file = open(self._hack_file_name, 'w')
-            self._hack_file.writelines(self._machine_code)
-            self._hack_file.close()
+        self._hack_file = open(self._hack_file_name, 'w')  # open the output hack file
+        self._hack_file.writelines(self._machine_code)  # write the machine code instructions in the file
+        self._hack_file.close()
 
-    # return True if the current line (instructions[line]) is a assembly instruction
+    # receive an assembly A-Instruction and return the 15-bits address
+    def _translate_a_instruction(self, instruction):
+        self._parser.set_a_instruction(instruction)
+        n = self._parser.number()
+        nn = self._code.number(n)
+        return nn
+
+    # receive an assembly C-Instruction and return a tuple with the 3 layers of machine language
+    def _translate_c_instruction(self, instruction):
+        self._parser.set_c_instruction(instruction)
+        c = self._parser.comp()
+        d = self._parser.dest()
+        j = self._parser.jump()
+        cc = self._code.comp(c)
+        dd = self._code.dest(d)
+        jj = self._code.jump(j)
+        return cc, dd, jj
+
+    # return True if the current line is a assembly instruction
     # return False if the current line is a blank line or a comment line
-    def _is_instruction(self):
-        instruction = self._instructions[self._line].replace(' ', '')
+    def _is_instruction(self, line):
+        instruction = line.replace(' ', '')
         return instruction != '' and instruction[0:2] != '//' and instruction[0] != '('
 
     # remove the in-line comments of a assembly instruction
-    def _remove_comment(self):
-        instruction = self._instructions[self._line].replace(' ', '')
+    def _remove_comment(self, line):
+        instruction = line.replace(' ', '')
         return instruction.split('//')[0]
 
     # return True if the instruction argument is a C-Instruction
     # return False if the instruction argument is a A-Instruction
-    @staticmethod
-    def _is_c_instruction(instruction):
+    def _is_c_instruction(self, instruction):
         return instruction[0] != '@'
 
 
 def main():
-    if len(sys.argv) != 2:
-        print('Usage: HackAssembler.py Xxx.asm')
-    else:
-        sys.setrecursionlimit(17000)
-        file_name = sys.argv[1]
-        HackAssembler(file_name)
+    try:
+        for file_name in sys.argv[1:]:
+            HackAssembler(file_name)
+    except FileNotFoundError:
+        print('The files must be in the same directory of HackAssembler.py ')
 
 
 main()
